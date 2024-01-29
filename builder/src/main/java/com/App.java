2f8063d548b4d81bfb73154746db81e1e30ebdc5
nick-ras <nickras10@gmail.com>
2024-01-29 16:42:29 +0100

    dependencies added and removed

--- builder/src/main/java/com/App.java
+++ builder/src/main/java/com/App.java
@@ -2,13 +2,11 @@ package com;
 
 //mvn clean install && java -jar target/abstract-factory-1.0-SNAPSHOT.jar 
 
-// package main;
-
 public class App {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World!");
 
-		// app.say_hello();
 	}
 
 }

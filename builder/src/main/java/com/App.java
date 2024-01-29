e88bdd176e020400f9a6224d7ac28536fc3ebe59
nick-ras <nickras10@gmail.com>
2024-01-29 15:43:16 +0100

    making builder design

---
+++ builder/src/main/java/com/App.java
@@ -0,0 +1,14 @@
+package com;
+
+//mvn clean install && java -jar target/abstract-factory-1.0-SNAPSHOT.jar 
+
+// package main;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// app.say_hello();
+	}
+
+}
